04576b2175d0e2f407ba0cce184dd944ab9bf224
Harish  Parab <50201375+HarishPrb28@users.noreply.github.com>
2023-01-29 19:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
 
+var list = document.getElementById("todoList");
+function addfunction(){
+	var value= document.getElementById("newTodoInput").value;
+	var li = document.createElement("LI");
+	var textNode = document.createTextNode(value);
+	li.appendChild(textNode);
+	list.appendChild(li);	
+
+	list.addEventListner("click",function(){
+	list.removeChild();
+	});
+	
+}
+
