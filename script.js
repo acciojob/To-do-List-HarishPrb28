6ba54c02830e4b976a6785e581ea9b5beabc3f1c
Harish  Parab <50201375+HarishPrb28@users.noreply.github.com>
2023-01-29 19:55:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 //your code here
 
-var list = document.getElementById("todoList");
-function addfunction(){
-	var value= document.getElementById("newTodoInput").value;
-	var li = document.createElement("LI");
-	var textNode = document.createTextNode(value);
-	li.appendChild(textNode);
-	list.appendChild(li);	
+// var list = document.getElementById("todoList");
+// function addfunction(){
+// 	var value= document.getElementById("newTodoInput").value;
+// 	var li = document.createElement("LI");
+// 	var textNode = document.createTextNode(value);
+// 	li.appendChild(textNode);
+// 	list.appendChild(li);	
 
-	list.addEventListner("click",function(){
-	list.removeChild();
-	});
+// 	list.addEventListner("click",function(){
+// 	list.removeChild();
+// 	});
 	
+// }
+
+function addList(){
+	var item= document.getElementById("newTodoInput");
+	var list = document.getElementById("todoList");
+	if(item.value !=""){
+		var li = document.createElement("LI");
+		li.appendChild(document.createTextNode(item.value));
+		list.appendChild(li);
+
+		item.value ="";
+		li.onclick = function(){
+        this.parentNode.removeChild(this);
+      }
+	}
+	else{
+ 
+    // Alert msg when value of box is "" empty.
+    alert("plz add a value to item");
+  }
 }
 
